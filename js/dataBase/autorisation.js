41e8960a7d94270972b8cfe7d94c357120ebefbb
InnaKononenko <108886768+InnaKononenko@users.noreply.github.com>
2022-08-24 18:41:58 +0300

    Add files via upload

---
+++ js/dataBase/autorisation.js
@@ -0,0 +1,14 @@
+export const autorisation = [
+  {
+    login: "inna",
+    password: "1234",
+  },
+  {
+    login: "elena",
+    password: "4321",
+  },
+  {
+    login: "anna",
+    password: "5678",
+  },
+];
